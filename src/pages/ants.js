9ac426cd0770cf33d5602a1f69bd9f0bd0c73bb0
Colin Dunn <colin.dunn15@gmail.com>
2022-05-11 10:06:14 -0400

    ant site styling and icon updates

--- src/pages/ants.js
+++ src/pages/ants.js
@@ -3,9 +3,11 @@ import styled from "styled-components";
 import dayjs from "dayjs";
 import { ColorGenerator } from "../utils/chance";
 import { Helmet } from "react-helmet";
-import ant from "../images/ant.png";
+import ant from "../images/ant.gif";
 import { groupBy } from "../utils/array";
 import Modal from "react-modal";
+
+import "../style/index.css"
 
 const StorageKey = "ants";
 
@@ -13,41 +15,29 @@ const Header = styled.div`
   display: flex;
   width: 100%;
   justify-content: space-between;
-  margin-bottom: 48px;
+  margin-bottom: 30px;
 `;
 
 const Title = styled.h1`
   margin: 0;
-`;
-
-const Ant = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: start;
-
-  margin-right: 16px;
-  padding: 16px;
-
-  &:last-of-type {
-    margin-right: 0;
-  }
-
-  color: ${(p) => p.color};
-  background-color: #${(p) => p.background};
+  font-weight: 300;
+  font-size: 16px;
+  letter-spacing: 5px;
 `;
 
 const Colony = styled.div`
   display: flex;
   flex-direction: column;
   align-items: start;
+  margin-right: -20px;
 `;
 
 const Row = styled.div`
   display: flex;
+  flex-wrap: wrap;
   width: 100%;
-  overflow-x: scroll;
 
-  margin-bottom: 32px;
+  margin-bottom: 0px;
 
   &:last-of-type {
     margin-bottom: 0;
@@ -61,6 +51,41 @@ const Row = styled.div`
   scrollbar-width: none; /* Firefox */
 `;
 
+const Ant = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+
+  margin-right: 2px;
+  margin-bottom: 2px;
+  padding: 10px;
+  padding-left: 8px;
+  width: 50px;
+
+  span {
+    font-size: 9px;
+  }
+
+  color: ${(p) => p.color};
+  background-color: #${(p) => p.background};
+  border-radius: 8px;
+`;
+
+const AntButton = styled.button`
+  border: none;
+  background: none;
+  padding: 0;
+  width: 20px;
+  height: 20px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+  }
+`
+
 const ModalContent = styled.div`
   width: 100%;
   height: 100%;
@@ -68,17 +93,22 @@ const ModalContent = styled.div`
   align-items: center;
   justify-content: center;
   position: relative;
+  font-weight: 300;
+  font-size: 14px;
 `;
 
 const ModalCopy = styled.span`
-  color: white;
+  color: #EDE6D3;
+  text-align: center;
+  line-height: 20px;
+  letter-spacing: 5px;
 `;
 
 const ModelCloseButton = styled.button`
   position: absolute;
   right: 32px;
   top: 32px;
-  color: white;
+  color: #EDE6D3;
   border: none;
   appearance: none;
   background: none;
@@ -92,7 +122,7 @@ const modalStyles = {
     left: "0",
     right: "0",
     bottom: "0",
-    backgroundColor: "green",
+    backgroundColor: "#525e28",
     border: "none",
   },
 };
@@ -140,8 +170,10 @@ export default function Ants() {
       </Helmet>
 
       <Header>
-        <Title>Lian's Title</Title>
-        <button onClick={openModal}>Ant</button>
+        <Title>new tab colony</Title>
+        <AntButton onClick={openModal}>
+          <img src={ant}/>
+        </AntButton>
       </Header>
 
       <Colony>
@@ -155,9 +187,9 @@ export default function Ants() {
                   color={ant.text}
                   background={ant.background}
                 >
-                  <span>{date.format("HH:mm:ss")}</span>
-                  <span>{date.format("ddd")}</span>
-                  <span>{date.format("MM/DD/YY")}</span>
+                  <span>{date.format("h:mma")}</span>
+                  <span>{date.format("ddd").toLowerCase()}</span>
+                  <span>{date.format("MMM D").toLowerCase()}</span>
                 </Ant>
               );
             })}
@@ -173,7 +205,7 @@ export default function Ants() {
       >
         <ModelCloseButton onClick={closeModal}>X</ModelCloseButton>
         <ModalContent>
-          <ModalCopy>Ants have a collection memory.</ModalCopy>
+          <ModalCopy>An ant colony has memories<br></br>that its individual members don’t have.</ModalCopy>
         </ModalContent>
       </Modal>
     </>
@@ -181,44 +213,37 @@ export default function Ants() {
 }
 
 const Colors = [
-  { background: "606c52", text: "black" },
-  { background: "826ca5", text: "black" },
+  { background: "606c52", text: "#EDE6D3" },
+  { background: "826ca5", text: "#EDE6D3" },
   { background: "d88727", text: "black" },
-  { background: "441f00", text: "black" },
+  { background: "441f00", text: "#EDE6D3" },
   { background: "8ec0e7", text: "black" },
   { background: "edf3ce", text: "black" },
   { background: "e3c6d4", text: "black" },
-  { background: "edf3ce", text: "black" },
-  { background: "606c52", text: "black" },
-  { background: "e3c6d4", text: "black" },
   { background: "dec982", text: "black" },
-  { background: "a26f5b", text: "black" },
+  { background: "a26f5b", text: "#EDE6D3" },
   { background: "f8cd99", text: "black" },
-  { background: "525e28", text: "black" },
+  { background: "525e28", text: "#EDE6D3" },
   { background: "899aa1", text: "black" },
   { background: "8ca871", text: "black" },
-  { background: "5f6e8b", text: "black" },
-  { background: "863f77", text: "black" },
+  { background: "5f6e8b", text: "#EDE6D3" },
   { background: "c8c260", text: "black" },
-  { background: "a26f5b", text: "black" },
   { background: "7eb7c0", text: "black" },
-  { background: "72593b", text: "black" },
-  { background: "82824a", text: "black" },
+  { background: "72593b", text: "#EDE6D3" },
+  { background: "82824a", text: "#EDE6D3" },
   { background: "aebf94", text: "black" },
-  { background: "385234", text: "black" },
+  { background: "385234", text: "#EDE6D3" },
   { background: "f0e07d", text: "black" },
   { background: "aaab5c", text: "black" },
   { background: "a89074", text: "black" },
-  { background: "2e6255", text: "black" },
   { background: "ce9c83", text: "black" },
   { background: "e7763a", text: "black" },
   { background: "a78b45", text: "black" },
   { background: "df9498", text: "black" },
   { background: "f7cfcc", text: "black" },
   { background: "e0b75b", text: "black" },
-  { background: "293d60", text: "black" },
-  { background: "3b678c", text: "black" },
+  { background: "293d60", text: "#EDE6D3" },
+  { background: "3b678c", text: "#EDE6D3" },
   { background: "a6b5c8", text: "black" },
   { background: "edc987", text: "black" },
-  { background: "c9545b", text: "black" },
 ];
